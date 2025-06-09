0345ce70cb2aecb2c77e7d70fe6325123e05488f
piyush01-01 <piyush24888@gmail.com>
2025-06-10 02:52:52 +0530

    yes

--- src/main/java/vithalVisions/demo/BusinessApplication.java
+++ src/main/java/vithalVisions/demo/BusinessApplication.java
@@ -2,17 +2,10 @@ package vithalVisions.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class BusinessApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(BusinessApplication.class);
-	}
-
+public class BusinessApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessApplication.class, args);
 	}
